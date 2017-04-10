0884a028cc3ce023ff585b9fc31d2360667217d2
Devin Breen <dbreen@groupon.com>
2017-04-10 16:25:53 -0500

    wip: handle reverse numeral notation

--- roman.go
+++ roman.go
@@ -1,5 +1,7 @@
 package roman
 
+import "fmt"
+
 //import "fmt"
 
 // allow conversion from int to roman numeral string
@@ -19,8 +21,34 @@ subtractive notation is used: as in this table:
 Number		4	9	40	90	400	900
 Notation	IV	IX	XL	XC	CD	CM
  */
+/*
+
+if
+
+ */
+
+var int_to_roman = map[int]string{
+	1: "I",
+	5: "V",
+	10: "X",
+	50: "L",
+	100: "C",
+	500: "D",
+	1000: "M",
+}
+
+var roman_to_int = map[string]int{
+	"I": 1,
+	"V": 5,
+	"X": 10,
+	"L": 50,
+	"C": 100,
+	"D": 500,
+	"M": 1000,
+}
 
 const (
+
 	iM = 1000
 	iD = 500
 	iC = 100
@@ -38,6 +66,23 @@ const (
 	sI = "I"
 )
 
+
+
+type romanStruct struct {
+	intVal int
+	romanVal string
+}
+
+var allNumerals = []romanStruct{
+	romanStruct{intVal: 1, romanVal: "I"},
+	romanStruct{intVal: 5, romanVal: "V"},
+	romanStruct{intVal: 10, romanVal: "X"},
+	romanStruct{intVal: 50, romanVal: "L"},
+	romanStruct{intVal: 100, romanVal: "C"},
+	romanStruct{intVal: 500, romanVal: "D"},
+	romanStruct{intVal: 1000, romanVal: "M"},
+}
+
 // TODO: turn string into rune
 func printNumerals(char string, freq int) string {
 	//fmt.Println(fmt.Sprintf("printing %s %d times", char, freq))
@@ -54,10 +99,23 @@ func To(in int) string {
 
 	if m := in / iM; m > 0 {
 		return printNumerals(sM, m) + To(in % iM)
+
 	} else if m := in / iD; m > 0 {
+
+		if m == 9 {
+			return fmt.Sprintf("%s%s", sC, sM) + To(in % iD)
+		} else if m == 4 {
+			return fmt.Sprintf("%s%s", sC, sD) + To(in % iD)
+		}
 		return printNumerals(sD, m) + To(in % iD)
+
+//		Number		4	9	40	90	400	900
+//		Notation	IV	IX	XL	XC	CD	CM
+
 	} else if m := in / iC; m > 0 {
+
 		return printNumerals(sC, m) + To(in % iC)
+
 	} else if m := in / iL; m > 0 {
 		return printNumerals(sL, m) + To(in % iL)
 	} else if m := in / iX; m > 0 {
--- roman_test.go
+++ roman_test.go
@@ -28,21 +28,33 @@ func TestTo(t *testing.T) {
 	 */
 
 	for input, want := range map[int]string{
-		1: "I",
-		2: "II",
-		3: "III",
-		4: "IIII",
-		5: "V",
-		6: "VI",
-		9: "VIIII",
-		10: "X",
-		11: "XI",
-		15: "XV",
-		50: "L",
-		51: "LI",
-		100: "C",
-		500: "D",
-		1000: "M",
+		//1: "I",
+		//2: "II",
+		//3: "III",
+		//4: "IIII",  // no reverse
+		4: "IV",  // reverse
+		//5: "V",
+		//6: "VI",
+		//9: "VIIII", 	// no reverse
+		9: "IX", 	// reverse
+		//10: "X",
+		//11: "XI",
+		//15: "XV",
+		40: "XL",
+		//50: "L",
+		//51: "LI",
+		90: "XC",
+		//100: "C",
+		400: "CD",
+		//500: "D",
+		900: "CM",
+		//1000: "M",
+		//1050: "ML",
+		//1051: "MLI",
+		//1100: "MC",
+		//1600: "MDC",
+		//2000: "MM",
+
 
 
 
